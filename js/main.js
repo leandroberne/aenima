cc107162811760b7e3d886d5819668c193c7459c
Leandro Berne <leandro@plw.com.ar>
2021-11-14 04:46:47 -0300

    Agregados estilos y clase active/inactive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const btnToggle = document.querySelector('#menu_burguer');
 const sidebar = document.querySelector('#sidebar');
 const header = document.querySelector('header');
+const btnClose = document.querySelector('.btnClose');
+const banner = document.querySelector('.banner-image');
+const footer = document.querySelector('footer');
 
 btnToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
-  sidebar.style.setProperty('position', 'absolute');
-  header.style.setProperty('position', 'static');
+  header.classList.toggle('inactive');
+  banner.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+});
+
+btnClose.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  header.classList.toggle('inactive');
+  banner.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
 });
