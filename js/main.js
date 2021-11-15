6784ac06728fba4d5134db0f1617c1abb4481c72
Leandro Berne <leandro@plw.com.ar>
2021-11-15 04:22:21 -0300

    Agregado seccion Blog, cards, css

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ const sidebar = document.querySelector('#sidebar');
 const header = document.querySelector('header');
 const btnClose = document.querySelector('.btnClose');
 const banner = document.querySelector('.banner-image');
+const main = document.querySelector('main');
 const footer = document.querySelector('footer');
 
 btnToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
   header.classList.toggle('inactive');
   banner.classList.toggle('inactive');
+  main.classList.toggle('inactive');
   footer.classList.toggle('inactive');
 });
 
@@ -16,5 +18,6 @@ btnClose.addEventListener('click', () => {
   sidebar.classList.toggle('active');
   header.classList.toggle('inactive');
   banner.classList.toggle('inactive');
+  main.classList.toggle('inactive');
   footer.classList.toggle('inactive');
 });
