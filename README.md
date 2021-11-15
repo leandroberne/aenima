8e2e7dc523c73e398879b55b4f6ce6260e250bf8
Leandro Berne <leandro@plw.com.ar>
2021-11-15 06:58:53 -0300

    Agregada funcionalidad botones/barra, estilos y update README

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Proyecto creado por Leandro Berne para la Prueba Front-end de AENIMA.
 
 ## Instrucciones
 
+Para descargar/clonar el proyecto desde GitHub desde una consola seguir esta instrucción:
+
+### `git clone https://github.com/leandroberne/aenima`
+
+Luego abrir el archivo `index.html` (del directorio "\aenima\") en un navegador, o ejecutar desde extensión Live Server.
+
 ## Herramientas utilizadas
 
 - HTML
 - CSS
+- JavaScript Vanilla (ES6)
 - Flexbox
 - Grid
 - Bootstrap
--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@ const btnClose = document.querySelector('.btnClose');
 const banner = document.querySelector('.banner-image');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
+const homeNavb = document.querySelector('#home_navb');
+const tendenciasNavb = document.querySelector('#tendencias_navb');
+const blogNavb = document.querySelector('#blog_navb');
+const homeSide = document.querySelector('#home_side');
+const tendenciasSide = document.querySelector('#tendencias_side');
+const blogSide = document.querySelector('#blog_side');
+
+document.addEventListener('DOMContentLoaded', () => {
+  homeNavb.style.color = '#2a3037';
+  tendenciasNavb.style.color = '#cbd0d3';
+  blogNavb.style.color = '#cbd0d3';
+});
 
 btnToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
@@ -20,4 +32,46 @@ btnClose.addEventListener('click', () => {
   banner.classList.toggle('inactive');
   main.classList.toggle('inactive');
   footer.classList.toggle('inactive');
+});
+
+homeSide.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  header.classList.toggle('inactive');
+  banner.classList.toggle('inactive');
+  main.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+});
+
+tendenciasSide.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  header.classList.toggle('inactive');
+  banner.classList.toggle('inactive');
+  main.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+});
+
+blogSide.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  header.classList.toggle('inactive');
+  banner.classList.toggle('inactive');
+  main.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+});
+
+homeNavb.addEventListener('click', () => {
+  homeNavb.style.color = '#2a3037';
+  tendenciasNavb.style.color = '#cbd0d3';
+  blogNavb.style.color = '#cbd0d3';
+});
+
+tendenciasNavb.addEventListener('click', () => {
+  homeNavb.style.color = '#cbd0d3';
+  tendenciasNavb.style.color = '#2a3037';
+  blogNavb.style.color = '#cbd0d3';
+});
+
+blogNavb.addEventListener('click', () => {
+  homeNavb.style.color = '#cbd0d3';
+  tendenciasNavb.style.color = '#cbd0d3';
+  blogNavb.style.color = '#2a3037';
 });
